0e1957c177e82372642d50beb8ddce44e308fe4b
Yul Kang <yulkang2019@gmail.com>
2025-06-10 11:47:19 +0900

    Initial Commit

---
+++ KBO_ALARM/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  KBO_ALARM
+//
+//  Created by Yul Kang on 6/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ KBO_ALARM/KBO_ALARMApp.swift
@@ -0,0 +1,17 @@
+//
+//  KBO_ALARMApp.swift
+//  KBO_ALARM
+//
+//  Created by Yul Kang on 6/10/25.
+//
+
+import SwiftUI
+
+@main
+struct KBO_ALARMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
