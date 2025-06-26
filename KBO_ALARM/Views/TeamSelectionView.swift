e4be1dd3c96902ed0b56434b68be333cdec9adc5
Yul Kang <yulkang2019@gmail.com>
2025-06-26 16:58:40 +0900

    Refactor: Move team selection logic to TeamSelectionView

---
+++ KBO_ALARM/Views/TeamSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  TeamSelectionView.swift
+//  KBO_ALARM
+//
+//  Created by Yul Kang on 6/26/25.
+//
+
+import SwiftUI
+
+struct TeamSelectionView: View {
+	var body: some View {
+			Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+	}
+}
+
+#Preview {
+	TeamSelectionView()
+}
