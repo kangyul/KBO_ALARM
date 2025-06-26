a23ade98168cef05caf59c0f10b0d7df471ad443
Yul Kang <yulkang2019@gmail.com>
2025-06-26 11:28:06 +0900

    Add: Team Select page UI

--- KBO_ALARM/Model/Team.swift
+++ KBO_ALARM/Model/Team.swift
@@ -15,14 +15,14 @@ struct Team: Identifiable {
 }
 
 let kboTeams = [
-	Team(id: 123, name: "LG 트윈스", color: Color(red: 165/256, green: 0, blue: 52/256), logo: "5002"),
-	Team(id: 123, name: "기아 타이거즈", color: Color(red: 234/256, green: 0, blue: 41/256), logo: "2002"),
-	Team(id: 123, name: "KT 위즈", color: Color(red: 0, green: 0, blue: 0), logo: "12001"),
-	Team(id: 123, name: "키움 히어로즈", color: Color(red: 87/256, green: 5/256, blue: 20/256), logo: "10001"),
-	Team(id: 123, name: "삼성 라이온즈", color: Color(red: 7/256, green: 76/256, blue: 161/256), logo: "1001"),
-	Team(id: 123, name: "한화 이글스", color: Color(red: 252/256, green: 78/256, blue: 0/256), logo: "3001"),
-	Team(id: 123, name: "두산 베어스", color: Color(red: 26/256, green: 23/256, blue: 72/256), logo: "6002"),
-	Team(id: 123, name: "NC 다이노스", color: Color(red: 49/256, green: 82/256, blue: 136/256), logo: "11001"),
-	Team(id: 123, name: "롯데 자이언츠", color: Color(red: 4/256, green: 30/256, blue: 66/256), logo: "3001"),
-	Team(id: 123, name: "SSG 랜더스", color: Color(red: 206/256, green: 14/256, blue: 45/256), logo: "9002")
+	Team(id: 1, name: "LG 트윈스", color: Color(red: 165/256, green: 0, blue: 52/256), logo: "5002"),
+	Team(id: 2, name: "기아 타이거즈", color: Color(red: 234/256, green: 0, blue: 41/256), logo: "2002"),
+	Team(id: 3, name: "KT 위즈", color: Color(red: 0, green: 0, blue: 0), logo: "12001"),
+	Team(id: 4, name: "키움 히어로즈", color: Color(red: 87/256, green: 5/256, blue: 20/256), logo: "10001"),
+	Team(id: 5, name: "삼성 라이온즈", color: Color(red: 7/256, green: 76/256, blue: 161/256), logo: "1001"),
+	Team(id: 6, name: "한화 이글스", color: Color(red: 252/256, green: 78/256, blue: 0/256), logo: "3001"),
+	Team(id: 7, name: "두산 베어스", color: Color(red: 26/256, green: 23/256, blue: 72/256), logo: "6002"),
+	Team(id: 8, name: "NC 다이노스", color: Color(red: 49/256, green: 82/256, blue: 136/256), logo: "11001"),
+	Team(id: 9, name: "롯데 자이언츠", color: Color(red: 4/256, green: 30/256, blue: 66/256), logo: "3001"),
+	Team(id: 10, name: "SSG 랜더스", color: Color(red: 206/256, green: 14/256, blue: 45/256), logo: "9002")
 ]
---
+++ KBO_ALARM/Views/ContentView.swift
@@ -0,0 +1,60 @@
+//
+//  ContentView.swift
+//  KBO_ALARM
+//
+//  Created by Yul Kang on 6/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+	@State private var myTeam: Team? = nil
+
+	let columns = [
+		GridItem(.flexible()),
+		GridItem(.flexible()),
+		GridItem(.flexible())
+	]
+
+	var body: some View {
+		VStack {
+			Text("나의 KBO 팀을 선택하세요")
+				.font(.title)
+				.fontWeight(.semibold)
+				.padding(.top, 16)
+
+			ScrollView {
+				LazyVGrid(columns: columns, spacing: 20) {
+					ForEach(kboTeams) { kboTeam in
+						Button(action: {
+							myTeam = kboTeam
+						}) {
+							ZStack {
+								VStack {
+									Image(kboTeam.logo)
+										.resizable()
+										.frame(width: 80, height: 80)
+								}
+								.padding()
+								.background(kboTeam.color)
+								.cornerRadius(8)
+								.opacity(myTeam?.id == kboTeam.id ? 0.5 : 1.0)
+
+								if myTeam?.id == kboTeam.id {
+									Image(systemName: "checkmark.circle.fill")
+										.font(.system(size: 50))
+										.foregroundColor(.white)
+								}
+							}
+						}
+					}
+				}
+				.padding()
+			}
+		}
+	}
+}
+
+#Preview {
+    ContentView()
+}
